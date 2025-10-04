f9ee1919f97c32d2afcd469a10569e272daef9cf
Stephen <stephen20120401@gmail.com>
2025-10-04 12:00:29 -0700

    added backround with overlay

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import styles from "./Homepage.module.css";
 
-const Homepage = () => {};
+const Homepage = () => {return (
+  <section className={styles.SplashImage}>
+    <div className={styles.overlay}>
+      <div>
+        <h1 className={styles.Title}>Base 3R4</h1>
+        <p className={styles.Des}>The future of recycling on another world</p>
+      </div>
+    </div>
+  </section>
+)};
 
 export default Homepage;
