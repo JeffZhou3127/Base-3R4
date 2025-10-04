372ca83aa67e985b40e1f0e866ab0d4ebe73f774
Jeff Zhou <zhouj0805@gmail.com>
2025-10-04 15:08:29 -0700

    Add OurMission and OurDesign components

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import styles from "./Homepage.module.css";
 
 import SplashImage from "../components/splashImage/SplashImage";
+import OurMission from "../components/ourMission/OurMission";
+import OurDesign from "../components/ourDesign/OurDesign";
 
 const Homepage = () => {
   return (
     <>
       <SplashImage />
+      <OurMission />
+      <OurDesign />
     </>
   );
 };
