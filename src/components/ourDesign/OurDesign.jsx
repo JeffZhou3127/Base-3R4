7978b682521871232ab97fd57cd48dd5275eb651
Jeff Zhou <zhouj0805@gmail.com>
2025-10-04 18:59:00 -0700

    Create Our Design and fix bugs

--- src/components/ourDesign/OurDesign.jsx
+++ src/components/ourDesign/OurDesign.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./OurDesign.module.css";
 import marsJezeroCraterJpg from "../../assets/mars_jezero_crater.jpg";
 
@@ -13,6 +14,29 @@ const OurDesign = () => {
         pre-launch logistics, our system aims to solve the inevitable problem of waste management on a planet
         220 million miles away.
       </p>
+      <div className={styles.container}>
+        <Link className={styles.cards}>
+          <h3>Problem 1: <span>Residence Renovations</span></h3>
+          <p>
+            Aluminum structures and the 3-dimensional structure used to inflate Martian habitats are difficult to recycle and
+            reuse in their current form without melting, which is unfeasible on Mars because of its lack of oxygen.
+          </p>
+        </Link>
+        <Link className={styles.cards}>
+          <h3>Problem 2: <span>Cosmic Celebrations</span></h3>
+          <p>
+            Plastics like Ethylene-Vinyl Acetate (EVA) waste and Polymer Matrix Composites are difficult to break up and recycle
+            because of their robust and reinforced molecular crosslinked systems.
+          </p>
+        </Link>
+        <Link className={styles.cards}>
+          <h3>Problem 3: <span>Daring Discoveries</span></h3>
+          <p>
+            Pure carbon from oxygen separation projects may not seem to amount to anything useful.
+            We don’t have the capability to transport such advanced technology to space to produce graphite from carbon.
+          </p>
+        </Link>
+      </div>
     </section>
   );
 };
